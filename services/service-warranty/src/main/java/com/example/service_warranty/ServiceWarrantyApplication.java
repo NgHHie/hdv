03ad7b0fa39af096739b05c10fe1f21c3735cb25
Nguyễn Hoàng Hiệp <hiep2003ka@gmail.com>
2025-04-29 18:58:23 +0700

    tạo base các service

---
+++ services/service-warranty/src/main/java/com/example/service_warranty/ServiceWarrantyApplication.java
@@ -0,0 +1,13 @@
+package com.example.service_warranty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceWarrantyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceWarrantyApplication.class, args);
+	}
+
+}
