952d7c51cc698229f1c8efb8f6418831b8e3c238
maybetuandat <trinhvinhtuandat05102003@gmail.com>
2025-05-01 13:27:56 +0700

    update and fix volume network

--- services/service-customer/src/main/java/com/example/service_customer/controllers/CustomerController.java
+++ services/service-customer/src/main/java/com/example/service_customer/controllers/CustomerController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/customers")
+@RequestMapping("/api/v1/customer")
 @RequiredArgsConstructor
 @Slf4j
 public class CustomerController {
