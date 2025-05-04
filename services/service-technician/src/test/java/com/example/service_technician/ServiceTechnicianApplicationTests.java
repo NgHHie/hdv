fe6491722731364da6a798740de9e541f75c97a1
Ha Cuong Thinh <thinhhacuong123@gmail.com>
2025-05-04 10:47:38 +0700

    add service technician

---
+++ services/service-technician/src/test/java/com/example/service_technician/ServiceTechnicianApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.service_technician;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceTechnicianApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
